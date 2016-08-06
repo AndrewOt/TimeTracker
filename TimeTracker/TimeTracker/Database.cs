a86bfaa4c38c340cae81fb27d22b813c8b611031
Andrew Ottaviano <Andrew Ottaviano>
2016-08-06 15:20:37 -0500

    Added new form and opened it on button click. I also made it so that it reads from the database.

---
+++ TimeTracker/TimeTracker/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker
+{
+	class DbConn
+	{
+		public static SQLiteDataReader doQuery()
+		{
+			SQLiteConnection m_dbConnection;
+			m_dbConnection = new SQLiteConnection("Data Source=C:\\Users\\andre\\Programming\\C#\\TimeTracker\\TimeTracker\\TimeTracker\\TimeTrackerDB.db;Version=3;");
+			m_dbConnection.Open();
+			SQLiteCommand c = new SQLiteCommand("SELECT * FROM Client", m_dbConnection);
+			SQLiteDataReader d = c.ExecuteReader();
+			m_dbConnection.Close();
+			return d;
+		}
+
+		public static void doUpdateQuery()
+		{
+			SQLiteConnection m_dbConnection;
+			m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+			m_dbConnection.Open();
+		}
+
+	}
+}
